ef21f0e5898e853d216c009e833a113638a21c96
imt1195 <imt1195@gmail.com>
2023-03-15 14:59:15 -0400

    initial commit, first try for three.js and 3d models

---
+++ threejs/README.md
@@ -0,0 +1,3 @@
+# Three.js playground
+
+npm install three
---
+++ threejs/test.js
@@ -0,0 +1,62 @@
+// import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+// const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+// const renderer = new THREE.WebGLRenderer();
+// renderer.setSize( window.innerWidth, window.innerHeight );
+// document.body.appendChild( renderer.domElement );
+
+// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+
+// camera.position.z = 5;
+
+// function animate() {
+//     requestAnimationFrame( animate );
+
+//     cube.rotation.x += 0.01;
+//     cube.rotation.y += 0.01;
+
+//     renderer.render( scene, camera );
+// }
+
+// animate();
+
+
+// testing with open source gltf files, saves local in assets folder
+// Instantiate a loader
+const loader = new GLTFLoader();
+
+// Load a glTF resource
+loader.load(
+	// resource URL
+	'./assets/plain.gltf',
+	// called when the resource is loaded
+	function ( gltf ) {
+
+		scene.add( gltf.scene );
+
+		gltf.animations; // Array<THREE.AnimationClip>
+		gltf.scene; // THREE.Group
+		gltf.scenes; // Array<THREE.Group>
+		gltf.cameras; // Array<THREE.Camera>
+		gltf.asset; // Object
+
+	},
+	// called while loading is progressing
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
